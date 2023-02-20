a4e9c7dbd6f740653c343637a2dbacf4d7e44c3e
mayumecristine <mayumecristine@gmail.com>
2023-02-20 08:21:37 -0300

    fix: modificando a margin

--- components/Resume.vue
+++ components/Resume.vue
@@ -56,9 +56,9 @@
       </div>
     </div>
     <div class="flex flex-col items-center">
-      <h3 class="text-2xl text-gray-700 font-bold mb-6 -ml-3 text-white text-center">Experiências</h3>
+      <h3 class="text-2xl text-gray-700 font-bold mb-6 ml-3 text-white text-center">Experiências</h3>
 
-      <ol class="border-l-2 border-purple-600">
+      <ol class="border-l-2 border-purple-600 m-7">
         <li>
           <div class="md:flex flex-start">
             <div class="bg-purple-600 w-6 h-6 flex items-center justify-center rounded-full -ml-3">
